0988967cd0d7e7c8c875e189a8db348d48247d4d
pavankat@gmail.com <pavankat@gmail.com>
2012-12-31 23:45:37 -0500

    working on problem3

--- problem3.rb
+++ problem3.rb
@@ -2,8 +2,23 @@
 	
 	Largest prime factor
 	Problem 3
+
 	The prime factors of 13195 are 5, 7, 13 and 29.
 
 	What is the largest prime factor of the number 600851475143 ?
 
 =end
+
+#lets find the largest 
+#prime factor of 147 = 3*7*7
+#which is 7
+
+number = 147
+
+(2..100).each do |i|
+
+	 if number % i == 0
+	 	puts "#{i}\n"
+	 end
+
+end
