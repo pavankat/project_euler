ddcc7a145ee3582753b2b3c52d654e7a15d79a77
pavankat@gmail.com <pavankat@gmail.com>
2013-01-01 23:31:12 -0500

    added comments and explanation

--- problem3.rb
+++ problem3.rb
@@ -9,16 +9,30 @@
 
 =end
 
+
 # defines constant N whose prime factors will be found
 N = 600851475143
+
+# compare will go down
 compare = N
+
+# initialize
 largestFactor = 0
- 
+
+# start with lowest prime number for counter
 counter = 2
+
+puts "counter is now #{counter}\n"
+puts "compare is now #{compare}\n"
+puts "largest factor is now #{largestFactor}\n\n\n"
+
 while (counter * counter) < compare
     if (compare % counter == 0) 
+    	puts "counter is now #{counter}\n"
         compare = compare / counter
+        puts "compare is now #{compare}\n"
         largestFactor = counter
+        puts "largest factor is now #{largestFactor}\n\n\n"
     else 
         counter += 1
     end
