4d6f6d96dd5645fdac0dd8f8af6c80758582a1a8
pavankat@gmail.com <pavankat@gmail.com>
2013-01-02 00:34:15 -0500

    got it to compile but gives wrong answer

--- problem4.rb
+++ problem4.rb
@@ -11,3 +11,32 @@
 
 =end
 
+# initialize the largest palindrome made from 
+# the product of two 3-digit numbers
+p = 0
+
+(100..999).each do |i|
+
+	(100..999).each do |j|
+
+		# figure out whether
+		# i*j is a palindrome
+		isp = i*j
+		isp = isp.to_s
+
+		if isp = isp.reverse
+			
+			isp = isp.to_i
+
+			# is isp larger than 
+			# the previous palindrome?
+			if isp > p
+				p = isp
+			end
+		end
+	end
+
+end
+
+puts "The largest palindrome made from the \
+product of two 3-digit numbers is #{p}"
