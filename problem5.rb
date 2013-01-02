66a8fc5bc801c06cb3d6f5d8d288ca148c9af737
pavankat@gmail.com <pavankat@gmail.com>
2013-01-02 01:17:43 -0500

    compiles but logic isnt right

--- problem5.rb
+++ problem5.rb
@@ -14,3 +14,25 @@ by all of the numbers from 1 to 20?
 
 =end
 
+# initialize 
+smallest = 3628800
+
+(3628800..20).each do |i|
+	
+	# initialize 
+	a = true
+
+	(2..10).each do |j|
+		if i % j != 0
+			a = false
+		end
+	end
+
+	if a == true
+		if i < smallest 
+			smallest = i
+		end
+	end
+end
+
+puts "smallest number #{smallest}"
