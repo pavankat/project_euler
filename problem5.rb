63d40b318c66c3fe632f6d6b1613cd8cffc89549
pavankat@gmail.com <pavankat@gmail.com>
2013-01-02 17:07:38 -0500

    finished with brute force

--- problem5.rb
+++ problem5.rb
@@ -14,27 +14,23 @@ by all of the numbers from 1 to 20?
 
 =end
 
-# initialize 
-smallest = 3628800
+# brute force :(
 
-(20..3628800).each do |i|
+i = 1
+ 
+while i %  2 != 0 || i %  3 != 0 || \
+	i %  4 != 0 || i %  5 != 0 || \
+	i %  6 != 0 || i %  7 != 0 || \
+	i %  8 != 0 || i %  9 != 0 || \
+	i % 10 != 0 || i % 11 != 0 || \
+	i % 12 != 0 || i % 13 != 0 || \
+	i % 14 != 0 || i % 15 != 0 || \
+	i % 16 != 0 || i % 17 != 0 || \
+	i % 18 != 0 || i % 19 != 0 || i % 20 != 0
 
-	# go through numbers 1 through 10
-	(1..10).each do |j| 
-
-		# if 1-10 are factors of j
-		# then a is false
-		if i % j != 0
-			a = false
-		end
-	end
-
-	if a == true
-		if i < smallest 
-			smallest = i
-		end
-	end
+    i += 1
 end
 
-# should return 2520
-puts "smallest number #{smallest}"
+puts i
+
+
